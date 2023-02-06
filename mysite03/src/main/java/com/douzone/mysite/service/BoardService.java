d2b3bc3487a0caac64ae874c0b48203607cc2ff1
kimki0917 <kimki0917@naver.com>
2023-02-06 09:22:05 +0900

    mysite02수정

--- mysite03/src/main/java/com/douzone/mysite/service/BoardService.java
+++ mysite03/src/main/java/com/douzone/mysite/service/BoardService.java
@@ -44,27 +44,35 @@ public class BoardService {
 		// 1.view 에서 게시판 리스트를 렌더링 하기 위한 데이터값 계산
 		int prevPage = page - 1;
 		int nextPage = page + 1;
+//		int endPage = ((totalPage < PAGE_SIZE)|| (page+PAGE_SIZE-1>=totalPage)) ? totalPage : page+PAGE_SIZE-1;
+//		int beginPage = (prevPage<=0||endPage-PAGE_SIZE+1<=0)?1:(endPage-PAGE_SIZE+1);
+		
+		//int endPage = ((totalPage < PAGE_SIZE)|| (nextPage>=totalPage)) ? totalPage : beginPage+PAGE_SIZE-1;
+//		if((totalPage < PAGE_SIZE)|| (nextPage>=totalPage)){
+//			endPage=totalPage;
+//			if(nextPage>=totalPage) {
+//				beginPage=totalPage-PAGE_SIZE+1;
+//			}
+//		}
 
-		int endPage = 2;
-		int beginPage = (page - 2) <= 0 ? 1 : page - 2;
-
-		if (page == 1 || page == 2) {
+//		int beginPage=page - 2;
+//		int endPage = page + 2;
+//		beginPage = (totalPage<PAGE_SIZE|| prevPage<=0||beginPage<=0)?1:page-2;
+//		endPage = (totalPage<=PAGE_SIZE||nextPage>=totalPage)? totalPage : beginPage+PAGE_SIZE-1;
+//		if(totalPage>PAGE_SIZE&&nextPage>=totalPage) {
+//			beginPage=endPage-PAGE_SIZE+1;
+//		}
+		//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+		int beginPage = page-2 <1 ? 1: page-2;
+		int endPage = page+2 > totalPage ? totalPage: page+2;
+		if(page <=2 && totalPage > PAGE_SIZE ) {
+			beginPage =1;
 			endPage = PAGE_SIZE;
-			if (endPage > totalPage) {
-				endPage = totalPage;
-			}
 		}
-
-		if (page + 2 > totalPage) {
-			endPage = totalPage;
-			beginPage = endPage - PAGE_SIZE + 1;
-			if (beginPage <= 0) {
-				beginPage = 1;
-			}
-		} else {
-			endPage = page + 2;
+		if(page >= endPage-2 && totalPage > PAGE_SIZE ) {
+			beginPage = endPage-PAGE_SIZE+1;
 		}
-
+		//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 		// 2. 리스트 가져오기
 		List<BoardVo> list = boardRepository.findAllByPageAndKeyWord(page - 1, keyword, LIST_SIZE);
 
